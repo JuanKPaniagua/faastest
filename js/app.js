44a87b581ae69eb53ee1937cbe1c586ed106ad79
JuanKPaniagua <47763268+JuanKPaniagua@users.noreply.github.com>
2020-11-12 22:20:47 -0600

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,61 +1,59 @@
 function getAll(entity) {
-    fetch('https://faas-juank.netlify.app/api/' + entity)
-      .then((response) => response.json())
-        .then((data) => {
-            fetch('/template/list/' + entity + '.html')
-                .then((response) => response.text())
-                .then((template) => {
-                    var rendered = Mustache.render(template, data);
-                    document.getElementById('content').innerHTML = rendered;
-                });
-        })
+	fetch('https://faas-example.netlify.app/api/' + entity)
+		.then((data) => {
+			fetch('/template/list/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
 }
 
 function getById(query, entity) {
-    var params = new URLSearchParams(query);
-    fetch('https://faas-juank.netlify.app/api/' + entity + '/?id=' + params.get('id'))
-      .then((response) => response.json())
-        .then((data) => {
-            fetch('/template/detail/' + entity + '.html')
-                .then((response) => response.text())
-                .then((template) => {
-                    var rendered = Mustache.render(template, data);
-                    document.getElementById('content').innerHTML = rendered;
-                });
-        })
+	var params = new URLSearchParams(query);
+	fetch('https://faas-example.netlify.app/api/' + entity + '/' + params.get('id'))
+		.then((data) => {
+			fetch('/template/detail/' + entity + '.html')
+				.then((response) => response.text())
+				.then((template) => {
+					var rendered = Mustache.render(template, data);
+					document.getElementById('content').innerHTML = rendered;
+				});
+		})
 }
 
 function home() {
-    fetch('/template/home.html')
-        .then((response) => response.text())
-        .then((template) => {
-            var rendered = Mustache.render(template, {});
-            document.getElementById('content').innerHTML = rendered;
-        });
+	fetch('/template/home.html')
+		.then((response) => response.text())
+		.then((template) => {
+			var rendered = Mustache.render(template, {});
+			document.getElementById('content').innerHTML = rendered;
+		});
 }
 
 function init() {
-    router = new Navigo(null, false, '#!');
-    router.on({
-        '/books': function() {
-            getAll('books');
-        },
-        '/authors': function() {
-            getAll('authors');
-        },
-        '/publishers': function() {
-            getAll('publishers');
-        },
-        '/bookById': function(_, query) {
-            getById(query, 'books');
-        },
-        '/authorById': function(_, query) {
-            getById(query, 'authors');
-        },
-        '/publisherById': function(_, query) {
-            getById(query, 'publishers');
-        }
-    });
-    router.on(() => home());
-    router.resolve();
+	router = new Navigo(null, false, '#!');
+	router.on({
+		'/books': function() {
+			getAll('books');
+		},
+		'/authors': function() {
+			getAll('authors');
+		},
+		'/publishers': function() {
+			getAll('publishers');
+		},
+		'/bookById': function(_, query) {
+			getById(query, 'books');
+		},
+		'/authorById': function(_, query) {
+			getById(query, 'authors');
+		},
+		'/publisherById': function(_, query) {
+			getById(query, 'publishers');
+		}
+	});
+	router.on(() => home());
+	router.resolve();
 }
